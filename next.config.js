b41ff0a5ed40b0a03d86da4f2707e00901a455dd
SakilTT <sakilsaiyed@techterminus.in>
2024-11-20 13:07:17 +0530

    js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const isProd = process.env.NODE_ENV === 'production';
 /* eslint-disable import/no-extraneous-dependencies */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
@@ -7,13 +8,14 @@ module.exports = withBundleAnalyzer({
   eslint: {
     dirs: ['.'],
   },
+  assetPrefix: isProd ? '/techterminus-next-master/' : '',
+  basePath: isProd ? '/techterminus-next-master' : '',
   images: {
     unoptimized: true,
   },
   output: 'export',
   poweredByHeader: false,
   trailingSlash: true,
-  basePath: '',
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
